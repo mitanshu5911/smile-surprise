2040f92ac9b3c13f90e171fcd1057404f5bebbe5
mitanshu5911 <mitanshubansal1234@gmail.com>
2026-03-15 14:59:13 +0530

    do

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 // import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base:"/",
   plugins: [react(),
     // tailwindcss()
   ],
 })
+
